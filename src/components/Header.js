292e31880932788a4fcbf4b50c8fe5fb7fad0730
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-07-02 23:51:16 +0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header className="Head">
+            <nav className="Head--nav">
+                <img src="../images/Trollface.png" alt="Trollface" className="nav--troll"/>
+                <h3 className="nav--gen">Meme Generator</h3>
+                <p className="nav--course">React Course - Project 3</p>
+            </nav>
+        </header>
+
+    );
+}
